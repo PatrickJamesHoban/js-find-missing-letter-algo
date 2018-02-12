510671d3a22362b292d709beea9f547a5967de22
PatrickJamesHoban <Patrick.J.Hoban@gmail.com>
2018-02-12 14:07:24 -0600

    updated readme with challenge description added js file

---
+++ find_missing_letter.js
@@ -0,0 +1,13 @@
+// PSEUDOCODE
+
+
+// CODE
+function findMissingLetter(array)
+{
+  return ' ';
+}
+
+// DRIVER CODE
+
+findMissingLetter(['a','b','c','d','f']);   // 'e'
+findMissingLetter(['O','Q','R','S']);       // 'P')
